e93e5bdf8811fbbfa0e1621bec54b7f537e16fe0
nisabmohd <nisabmohd@gmail.com>
2023-02-25 00:15:40 +0530

    few changes in folders and file fetching

--- client/src/components/Cardc.js
+++ client/src/components/Cardc.js
@@ -1,32 +1,39 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import ImageIcon from '@mui/icons-material/Image';
-import { Box } from '@mui/system';
-import altimg from '../assets/alt.png'
-import altimg1 from '../assets/alt1.png'
-import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
-import GridOnIcon from '@mui/icons-material/GridOn';
-import MovieCreationIcon from '@mui/icons-material/MovieCreation';
-import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
-import { AppContext } from '../App';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Menu, MenuItem } from '@mui/material';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import axios from 'axios';
-import { url } from '../BaseUrl';
-import StarIcon from '@mui/icons-material/Star';
+import React, { useContext, useEffect, useRef, useState, useMemo } from "react";
+import ImageIcon from "@mui/icons-material/Image";
+import { Box } from "@mui/system";
+import altimg from "../assets/alt.png";
+import altimg1 from "../assets/alt1.png";
+import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+import GridOnIcon from "@mui/icons-material/GridOn";
+import MovieCreationIcon from "@mui/icons-material/MovieCreation";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import { AppContext } from "../App";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Menu, MenuItem } from "@mui/material";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import axios from "axios";
+import { url } from "../BaseUrl";
+import StarIcon from "@mui/icons-material/Star";
+import DownloadIcon from "@mui/icons-material/Download";
+import {
+  getStorage,
+  ref as refStorage,
+  getDownloadURL,
+} from "firebase/storage";
 
 export const Cardc = (props) => {
-  const context = useContext(AppContext)
+  const storage = useMemo(() => getStorage(), []);
+  const context = useContext(AppContext);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
-  const [isStar, setIsStar] = useState(false)
-  const ref = useRef()
+  const [isStar, setIsStar] = useState(false);
+  const ref = useRef();
 
   useEffect(() => {
-    setIsStar(props.star)
-  }, [props.star])
-  
+    setIsStar(props.star);
+  }, [props.star]);
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -35,30 +42,43 @@ export const Cardc = (props) => {
   };
 
   const handleStar = () => {
-    axios.put(`${url}/ff/starhandle`, {
-      uid: context.auth.uid,
-      fileid: props.id
-    })
+    axios
+      .put(`${url}/ff/starhandle`, {
+        uid: context.auth.uid,
+        fileid: props.id,
+      })
       .then(function (response) {
         console.log(response);
-        setIsStar(!isStar)
+        setIsStar(!isStar);
       })
       .catch(function (error) {
         console.log(error);
       });
-  }
+  };
 
   const handleDetele = () => {
-    axios.put(`${url}/ff/trashhandle`, {
-      uid: context.auth.uid,
-      fileid: props.id
-    })
+    axios
+      .put(`${url}/ff/trashhandle`, {
+        uid: context.auth.uid,
+        fileid: props.id,
+      })
       .then(function (response) {
         console.log(response);
-        props.handleRemoveFileCard(props.id)
-        // ref.current.style.display = 'none';
+        props.handleRemoveFileCard(props.id);
       })
       .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  async function handleDownload() {
+    const fileRef = refStorage(storage, `files/${props.name}`);
+    getDownloadURL(fileRef)
+      .then((url) => {
+        axios.get(url);
+        window.open(url, "_blank");
+      })
+      .catch((error) => {
         console.log(error);
       });
   }
@@ -66,61 +86,211 @@ export const Cardc = (props) => {
   console.log(props.trash, props.star);
   return (
     <div ref={ref}>
-      <Box className='card' sx={{ border: 1, borderColor: 'divider' }} style={{ width: '234px', height: '150px', borderRadius: '5px', display: 'flex', flexDirection: 'column', marginRight: '13px', marginTop: '19px', overflow: 'hidden', position: 'relative' }}>
-        <a href={`${props.link}`} target="_blank" rel="noreferrer" style={{ textDecoration: 'none', color: 'inherit',height:'inherit' }}>
-          <div href={props.url} className='card' style={{ position: 'relative', background: `url(${props.url})`, overflow: 'hidden', height: '74%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            {
-              (props.type === 'image/png' || props.type === 'image/jpeg' || props.type === 'image/jpg') ? <>
-                <img src={props.img} alt="" style={{ width: '100%' }} />
-              </> : <><img src={context.dark ? altimg1 : altimg} alt="" style={{ width: '100%',marginBottom:'-25px'}} /></>
-            }
+      <Box
+        className="card"
+        sx={{ border: 1, borderColor: "divider" }}
+        style={{
+          width: "234px",
+          height: "150px",
+          borderRadius: "5px",
+          display: "flex",
+          flexDirection: "column",
+          marginRight: "13px",
+          marginTop: "19px",
+          overflow: "hidden",
+          position: "relative",
+        }}
+      >
+        <a
+          href={`${props.link}`}
+          target="_blank"
+          rel="noreferrer"
+          style={{
+            textDecoration: "none",
+            color: "inherit",
+            height: "inherit",
+          }}
+        >
+          <div
+            href={props.url}
+            className="card"
+            style={{
+              position: "relative",
+              background: `url(${props.url})`,
+              overflow: "hidden",
+              height: "74%",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {props.type === "image/png" ||
+            props.type === "image/jpeg" ||
+            props.type === "image/jpg" ? (
+              <>
+                <img src={props.img} alt="" style={{ width: "100%" }} />
+              </>
+            ) : (
+              <>
+                <img
+                  src={context.dark ? altimg1 : altimg}
+                  alt=""
+                  style={{ width: "100%", marginBottom: "-25px" }}
+                />
+              </>
+            )}
           </div>
         </a>
-        <MoreVertIcon style={{ position: 'absolute', right: '3px', bottom: '11px', fontSize: '16.75px', zIndex: '9999', cursor: 'pointer' }} onClick={handleClick} />
+        <MoreVertIcon
+          style={{
+            position: "absolute",
+            right: "3px",
+            bottom: "11px",
+            fontSize: "16.75px",
+            zIndex: "9999",
+            cursor: "pointer",
+          }}
+          onClick={handleClick}
+        />
         <Menu
           id="basic-menu"
           anchorEl={anchorEl}
           open={open}
           onClose={handleClose}
           MenuListProps={{
-            'aria-labelledby': 'basic-button',
+            "aria-labelledby": "basic-button",
+          }}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
           }}
         >
-          <MenuItem onClick={handleStar} sx={{ fontFamily: 'Roboto Flex' }} >{!isStar ? <StarBorderIcon sx={{ width: '20px', height: '19px', fontSize: '14px', marginRight: '6px' }} /> : <StarIcon sx={{ width: '20px', height: '19px', fontSize: '14px', marginRight: '6px', color: '#ffcd3c' }} />}<p>{isStar ? "Unstar" : "Star"}</p></MenuItem>
-          <MenuItem onClick={handleDetele} sx={{ fontFamily: 'Roboto Flex', }}><DeleteOutlineIcon sx={{ width: '20px', height: '19px', marginRight: '6px', color: 'red' }} /><p>Remove</p></MenuItem>
+          <MenuItem onClick={handleStar} sx={{ fontFamily: "Roboto Flex" }}>
+            {!isStar ? (
+              <StarBorderIcon
+                sx={{
+                  width: "20px",
+                  height: "19px",
+                  fontSize: "14px",
+                  marginRight: "6px",
+                }}
+              />
+            ) : (
+              <StarIcon
+                sx={{
+                  width: "20px",
+                  height: "19px",
+                  fontSize: "14px",
+                  marginRight: "6px",
+                  color: "#ffcd3c",
+                }}
+              />
+            )}
+            <p>{isStar ? "Unstar" : "Star"}</p>
+          </MenuItem>
+          <MenuItem onClick={handleDownload} sx={{ fontFamily: "Roboto Flex" }}>
+            <DownloadIcon
+              sx={{
+                width: "20px",
+                height: "19px",
+                marginRight: "6px",
+              }}
+            />
+            <p>Download</p>
+          </MenuItem>
+          <MenuItem onClick={handleDetele} sx={{ fontFamily: "Roboto Flex" }}>
+            <DeleteOutlineIcon
+              sx={{
+                width: "20px",
+                height: "19px",
+                marginRight: "6px",
+                color: "red",
+              }}
+            />
+            <p>Remove</p>
+          </MenuItem>
         </Menu>
-        <a href={`${props.link}`} target="_blank" rel="noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
-          <div className='folder' style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '234px', height: '39px', marginRight: '13px', position: 'absolute', bottom: '0',backgroundColor:'inherit',zIndex:'55' }}>
-            {
-              (props.type === 'image/png' || props.type === 'image/jpeg' || props.type === 'image/jpg') ?
-                <ImageIcon style={{ width: '20px', marginLeft: '9px' }} /> : <>
-                </>
-            }
-            {
-              (props.type === 'application/pdf') ?
-                <PictureAsPdfIcon style={{ width: '20px', marginLeft: '9px', color: 'red' }} /> : <></>
+        <a
+          href={`${props.link}`}
+          target="_blank"
+          rel="noreferrer"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <div
+            className="folder"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              width: "234px",
+              height: "39px",
+              marginRight: "13px",
+              position: "absolute",
+              bottom: "0",
+              backgroundColor: "inherit",
+              zIndex: "55",
+            }}
+          >
+            {props.type === "image/png" ||
+            props.type === "image/jpeg" ||
+            props.type === "image/jpg" ? (
+              <ImageIcon style={{ width: "20px", marginLeft: "9px" }} />
+            ) : (
+              <></>
+            )}
+            {props.type === "application/pdf" ? (
+              <PictureAsPdfIcon
+                style={{ width: "20px", marginLeft: "9px", color: "red" }}
+              />
+            ) : (
+              <></>
+            )}
+            {props.type ===
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ? (
+              <GridOnIcon
+                style={{ width: "20px", marginLeft: "9px", color: "green" }}
+              />
+            ) : (
+              <></>
+            )}
+            {props.type === "video/x-matroska" ||
+            props.type === "video/mp4" ||
+            props.type === "video/3gp" ? (
+              <MovieCreationIcon
+                style={{ width: "20px", marginLeft: "9px", color: "#757ce8" }}
+              />
+            ) : (
+              <></>
+            )}
+            {props.type !== "video/x-matroska" &&
+            props.type !== "video/mp4" &&
+            props.type !== "video/3gp" &&
+            props.type !==
+              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" &&
+            props.type !== "application/pdf" &&
+            props.type !== "image/png" &&
+            props.type !== "image/jpeg" &&
+            props.type !== "image/jpg" ? (
+              <InsertDriveFileIcon
+                style={{ width: "20px", marginLeft: "9px" }}
+              />
+            ) : (
+              <></>
+            )}
 
-            }
-            {
-              (props.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') ?
-                <GridOnIcon style={{ width: '20px', marginLeft: '9px', color: 'green' }} /> : <></>
-
-            }
-            {
-              (props.type === 'video/x-matroska' || props.type === 'video/mp4' || props.type === 'video/3gp') ?
-                <MovieCreationIcon style={{ width: '20px', marginLeft: '9px', color: '#757ce8' }} /> : <></>
-
-            }
-            {
-              (props.type !== 'video/x-matroska' && props.type !== 'video/mp4' && props.type !== 'video/3gp' && props.type !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' && props.type !== 'application/pdf' && props.type !== 'image/png' && props.type !== 'image/jpeg' && props.type !== 'image/jpg') ?
-                <InsertDriveFileIcon style={{ width: '20px', marginLeft: '9px' }} /> : <></>
-            }
-
-            <p style={{ marginLeft: '9px', color: 'inherit' }}>{props.name?.length > 25 ? props.name?.slice(0, 20) + "..." : props.name}</p>
+            <p style={{ marginLeft: "9px", color: "inherit" }}>
+              {props.name?.length > 25
+                ? props.name?.slice(0, 20) + "..."
+                : props.name}
+            </p>
           </div>
         </a>
-      </Box >
+      </Box>
     </div>
-
-  )
-}
+  );
+};
--- server/routes/FilesAndFolders.js
+++ server/routes/FilesAndFolders.js
@@ -1,119 +1,151 @@
-const router = require('express').Router();
-const { FolderModel } = require('../models/Folder')
-const { FileModel } = require('../models/File')
-const { v4: folderId } = require('uuid');
+const router = require("express").Router();
+const { FolderModel } = require("../models/Folder");
+const { FileModel } = require("../models/File");
+const { v4: folderId } = require("uuid");
 
-router.post('/addfolder', async (req, res) => {
-    const { folderid, newfoldername, uid } = req.body
-    try {
-        const findFolder = await FolderModel.findOne({ Folderid: folderid })
-        if (!findFolder) return res.status(400)
-        const folderiduid = folderId()
-        const newFolder = new FolderModel({
-            Folderid: folderiduid,
-            Name: newfoldername,
-            uid: uid
-        })
-        await newFolder.save()
-        await FolderModel.updateOne({ Folderid: folderid }, { $push: { Folders: folderiduid } })
-        res.status(200).send({ newFolder: folderiduid });
-    } catch (err) {
-        res.status(400).send(err);
-    }
-})
+router.post("/addfolder", async (req, res) => {
+  const { folderid, newfoldername, uid } = req.body;
+  try {
+    const findFolder = await FolderModel.findOne({ Folderid: folderid });
+    if (!findFolder) return res.status(400);
+    const folderiduid = folderId();
+    const newFolder = new FolderModel({
+      Folderid: folderiduid,
+      Name: newfoldername,
+      uid: uid,
+    });
+    await newFolder.save();
+    await FolderModel.updateOne(
+      { Folderid: folderid },
+      { $push: { Folders: folderiduid } }
+    );
+    res.status(200).send({ newFolder: folderiduid });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-router.post('/addFile', async (req, res) => {
-    const { folderid, filename, storageLink, uid, filetype } = req.body
-    try {
-        const findFolder = await FolderModel.findOne({ Folderid: folderid })
-        if (!findFolder) return res.status(400)
-        const newfileid = folderId()
-        const newFile = new FileModel({
-            filename: filename,
-            fileid: newfileid,
-            filetype: filetype,
-            storageLink: storageLink,
-            uid: uid,
-            parentFolder: folderid
-        })
-        await newFile.save()
-        await FolderModel.updateOne({ Folderid: folderid }, { $push: { Files: newfileid } })
-        res.status(200).send({ newFile: newfileid });
-    } catch (err) {
-        console.log(err);
-        res.status(400).send(err);
-    }
-})
+router.post("/addFile", async (req, res) => {
+  const { folderid, filename, storageLink, uid, filetype } = req.body;
+  try {
+    const findFolder = await FolderModel.findOne({ Folderid: folderid });
+    if (!findFolder) return res.status(400);
+    const newfileid = folderId();
+    const newFile = new FileModel({
+      filename: filename,
+      fileid: newfileid,
+      filetype: filetype,
+      storageLink: storageLink,
+      uid: uid,
+      parentFolder: folderid,
+    });
+    await newFile.save();
+    await FolderModel.updateOne(
+      { Folderid: folderid },
+      { $push: { Files: newfileid } }
+    );
+    res.status(200).send({ newFile: newfileid });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+});
 
-router.get('/:folderid', async (req, res) => {
-    const findFolder = await FolderModel.findOne({ Folderid: req.params.folderid })
-    if (!findFolder) return res.status(400)
-    const Folders = findFolder.Folders
-    const Files = findFolder.Files
-    let explorefolder = []
-    await Promise.all(Folders.map(async (item) => {
-        const texp = await FolderModel.findOne({ Folderid: item }, { Folders: 0, Files: 0, uid: 0 })
-        explorefolder.push(texp)
-    }))
-    let exploreFiles = []
-    await Promise.all(Files.map(async (item) => {
-        const fexp = await FileModel.findOne({ fileid: item })
-        if (fexp.trash !== true)
-            exploreFiles.push(fexp)
-    }))
-    res.send({ folders: explorefolder, files: exploreFiles })
-})
-
-router.get('/starred/:uid', async (req, res) => {
-    const { uid } = req.params;
-    const starred = await FileModel.find({ uid: uid, starred: true, trash: { $ne: true } })
-    res.send(starred)
-})
-
-router.get('/trash/:uid', async (req, res) => {
-    const { uid } = req.params;
-    const trash = await FileModel.find({ uid: uid, trash: true })
-    res.send(trash)
-})
-
-router.get('/recent/:uid', async (req, res) => {
-    const { uid } = req.params;
-    const recent = await FileModel.find({ uid: uid, trash: { $ne: true } })
-    recent.sort((a, b) => {
-        -1 * (Date.parse(b) - Date.parse(a))
+router.get("/:folderid", async (req, res) => {
+  const findFolder = await FolderModel.findOne({
+    Folderid: req.params.folderid,
+  }).sort({ _id: 1 });
+  if (!findFolder) return res.status(400);
+  const Folders = findFolder.Folders;
+  const Files = findFolder.Files;
+  let explorefolder = [];
+  await Promise.all(
+    Folders.map(async (item) => {
+      const texp = await FolderModel.findOne(
+        { Folderid: item },
+        { Folders: 0, Files: 0, uid: 0 }
+      );
+      explorefolder.push(texp);
     })
-    res.send(recent)
-})
+  );
+  let exploreFiles = [];
+  await Promise.all(
+    Files.map(async (item) => {
+      const fexp = await FileModel.findOne({ fileid: item });
+      if (fexp.trash !== true) exploreFiles.push(fexp);
+    })
+  );
+  res.send({ folders: explorefolder, files: exploreFiles });
+});
 
-router.put('/starhandle', async (req, res) => {
-    const { fileid, uid } = req.body
-    const fileSearch = await FileModel.findOne({ fileid: fileid })
-    if (fileSearch.uid === uid) {
-        return res.send(await FileModel.updateOne({ fileid: fileid }, { starred: !fileSearch.starred }))
-    } else {
-        return res.status(401).send("Unauthorized")
+router.get("/starred/:uid", async (req, res) => {
+  const { uid } = req.params;
+  const starred = await FileModel.find({
+    uid: uid,
+    starred: true,
+    trash: { $ne: true },
+  });
+  res.send(starred);
+});
+
+router.get("/trash/:uid", async (req, res) => {
+  const { uid } = req.params;
+  const trash = await FileModel.find({ uid: uid, trash: true });
+  res.send(trash);
+});
+
+router.get("/recent/:uid", async (req, res) => {
+  const { uid } = req.params;
+  const recent = await FileModel.find({ uid: uid, trash: { $ne: true } });
+  recent.sort((a, b) => {
+    -1 * (Date.parse(b) - Date.parse(a));
+  });
+  res.send(recent);
+});
+
+router.put("/starhandle", async (req, res) => {
+  const { fileid, uid } = req.body;
+  const fileSearch = await FileModel.findOne({ fileid: fileid });
+  if (fileSearch.uid === uid) {
+    return res.send(
+      await FileModel.updateOne(
+        { fileid: fileid },
+        { starred: !fileSearch.starred }
+      )
+    );
+  } else {
+    return res.status(401).send("Unauthorized");
+  }
+});
+
+router.put("/trashhandle", async (req, res) => {
+  const { fileid, uid } = req.body;
+  const fileSearch = await FileModel.findOne({ fileid: fileid });
+  if (fileSearch.uid === uid) {
+    if (fileSearch.trash == true) {
+      await FileModel.deleteOne({ fileid: fileid });
+      await FolderModel.updateOne(
+        { Folderid: fileSearch.parentFolder },
+        { $pull: { Files: fileid } }
+      );
     }
-})
+    return res.send(
+      await FileModel.updateOne({ fileid: fileid }, { trash: true })
+    );
+  } else {
+    return res.status(401).send("Unauthorized");
+  }
+});
 
-router.put('/trashhandle', async (req, res) => {
-    const { fileid, uid } = req.body
-    const fileSearch = await FileModel.findOne({ fileid: fileid })
-    if (fileSearch.uid === uid) {
-        if (fileSearch.trash == true) {
-            await FileModel.deleteOne({ fileid: fileid })
-            await FolderModel.updateOne({ Folderid: fileSearch.parentFolder }, { $pull: { Files: fileid } })
-        }
-        return res.send(await FileModel.updateOne({ fileid: fileid }, { trash: true }))
-    } else {
-        return res.status(401).send("Unauthorized")
-    }
-})
+router.post("/search", async (req, res) => {
+  const { query } = req.body;
+  const files = await FileModel.find({
+    filename: { $regex: new RegExp(query.toLowerCase()) },
+  });
+  const folders = await FolderModel.find({
+    Name: { $regex: new RegExp(query.toLowerCase()) },
+  });
+  res.send({ files, folders });
+});
 
-router.post('/search', async (req, res) => {
-    const { query } = req.body
-    const files=await FileModel.find({ filename: {$regex:new RegExp(query.toLowerCase())} })
-    const folders=await FolderModel.find({ Name: {$regex:new RegExp(query.toLowerCase())} })
-    res.send({files,folders})
-})
-
-module.exports = router
+module.exports = router;
